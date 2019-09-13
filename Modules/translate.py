cea3416b2048e09bb517de06ae55f24a5c80f8cf
Zhack47 <54006084+Zhack47@users.noreply.github.com>
2019-09-13 19:43:41 +0200

    Update translate.py

--- Modules/translate.py
+++ Modules/translate.py
@@ -1,25 +1,31 @@
 from googletrans import Translator
 import Modules.audio_ui as audio_ui
 import Modules.IOverify as IO
+import Modules.lang_dict as switch_lang
 translator = Translator()
+
+def tr(data):
+    data = " ".join(data)
+    print(translator.detect(data).lang)
+    return translator.detect(data).lang
 def from_audio_translate_to_str(Audiostr, target_lang):
+    Audiostr = " ".join(Audiostr)
     language = translator.detect(Audiostr).lang
     strin = translator.translate(Audiostr, src=language, dest=target_lang).text
     strin = str(strin)
-    audio_ui.speak(strin)
     return strin
 
 
 def translate_data(data):
-    target_lang = acknowledge_output_language(data)
+    target_lang = switch_lang.get_lang_symbol(str(acknowledge_output_language(data)).capitalize())
     if target_lang == None:
         return from_audio_translate_to_str(data, 'fr')
     else:
-        return from_audio_translate_to_str(data, target_lang)
+        return from_audio_translate_to_str(data[2:], target_lang)
 
 
 def acknowledge_output_language(data):
     if "en" in data:
         data = IO.treat_intent('en', data)
-        return data[1]
+        return data[0]
     return None
