92f83fc9cfc2a0c28550caeb73f78b47f99dca23
zhack <mesbah.zacharia@sfr.fr>
2019-09-23 20:53:26 +0200

    lang

--- Modules/translate.py
+++ Modules/translate.py
@@ -4,10 +4,13 @@ import Modules.IOverify as IO
 import Modules.lang_dict as switch_lang
 translator = Translator()
 
+
 def tr(data):
     data = " ".join(data)
     print(translator.detect(data).lang)
     return translator.detect(data).lang
+
+
 def from_audio_translate_to_str(Audiostr, target_lang):
     Audiostr = " ".join(Audiostr)
     language = translator.detect(Audiostr).lang
@@ -28,8 +31,4 @@ def acknowledge_output_language(data):
     if "en" in data:
         data = IO.treat_intent('en', data)
         return data[0]
-<<<<<<< HEAD
-    return None
-=======
-    return None
->>>>>>> d7c918f93b279ee006c0b5ed6ee777168ecc4ff2
+    return 'fr'
